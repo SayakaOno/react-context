a50bd1e545bc1cae88c33cc48904f5d4f88afef5
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-07-27 11:13:42 -0700

    update context from Button component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toolbar from './Toolbar';
 
-export const ThemeContext = React.createContext();
+export const ThemeContext = React.createContext({
+  theme: 'blue',
+  changeTheme: () => {}
+});
 
 const App = () => {
+  const changeTheme = theme => setTheme(theme);
+  const [theme, setTheme] = useState('blue');
+
   return (
-    <ThemeContext.Provider value={'blue'}>
+    <ThemeContext.Provider value={{ theme, changeTheme }}>
       <Toolbar />
     </ThemeContext.Provider>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,21 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { ThemeContext } from './App';
 
 const Button = () => {
-  const theme = useContext(ThemeContext);
-  return <button style={{ backgroundColor: theme }}>Click!</button>;
+  return (
+    <ThemeContext.Consumer>
+      {({ theme, changeTheme }) => {
+        return (
+          <button
+            onClick={() => changeTheme('yellow')}
+            style={{ backgroundColor: theme }}
+          >
+            Click!
+          </button>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
 };
 
 export default Button;
